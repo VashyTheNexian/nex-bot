b110027f12a55ccc9e1b011e38ab68350c16f094
VashyTheNexian <hgeorge2009@gmail.com>
2012-06-25 18:32:46 -0400

    Trying to have the bot respond to commands

--- nex-bot.py
+++ nex-bot.py
@@ -18,7 +18,7 @@ import ConfigParser
 #PINGs in the same way with a certain interval, which have to be replied in the same way.
 #**************************************************************************************
 def sUSERNICK(s, NICK):
-	s.send('NICK '+NICK + "\r\n")
+	s.sendall('NICK '+NICK + "\r\n")
 	
 #**************************************************************************************
 # This function returns the desired value from the config file
@@ -45,6 +45,7 @@ Config.read(configFilename)
 
 #**************************************************************************************
 # Pull data from config file
+# Example can be found here: http://wiki.python.org/moin/ConfigParserExamples
 #**************************************************************************************
 HOST = ConfigSectionMap('irc')['host']
 NICK = ConfigSectionMap('irc')['nick']
@@ -53,49 +54,66 @@ CHANNELINIT = ConfigSectionMap('irc')['channelinit']
 REALNAME = ConfigSectionMap('irc')['realname']
 PASS = ConfigSectionMap('irc')['pass']
 
+
 #**************************************************************************************
 # Create socket and connect to server
 #**************************************************************************************
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((HOST, PORT))
-
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	s.connect((HOST, PORT))
+except Exception, err:
+	print "Error connecting to " + HOST
+	print str(err)
+	print(str(s.close()))
+	print str(s)
 #**************************************************************************************
 # If socket is null, exit
 #**************************************************************************************
 if s is None:
 	print "Socket error while connecting"
 	exit()
-
-try:
-	try :
-		line = s.recv(512) #s.recv() will never return None
-		while (line != ""):
-			print line #server message is output to screen
-			if line.find("No ident response") != -1:
-				sUSERNICK(s, NICK)
+if s:
+	try:
+		try :
+			line = s.recv(512)
+			while (line != ""):
+			
+				print line #server message is output to console
 				
-			if line.find("PING :") != -1:
-				pingrequest = re.search("PING :(.+)", line)
-				s.send("PONG :" + pingrequest.group(1))
-				s.send('USER ' +NICK + ' ' + NICK + ' ' + NICK + ' : ' + REALNAME + '\r\n')
-				s.send('AUTH ' + NICK + ' ' + PASS + '\r\n')
-				s.send('JOIN #'+CHANNELINIT + "\r\n") #Joins default channel
+				if line.find("No ident response") != -1:
+					sUSERNICK(s, NICK)
 				
-			if line.find("001") != -1:
-				print "Successfully connected!"
+				if line.find("PING :") != -1:
+					pingrequest = re.search("PING :(.+)", line)
+					s.sendall("PONG :" + pingrequest.group(1))
+					s.sendall('USER ' +NICK + ' ' + NICK + ' ' + NICK + ' : ' + REALNAME + '\r\n')
+					s.sendall('AUTH ' + NICK + ' ' + PASS + '\r\n')
+					
+				if line.find("001") != -1:
+					s.sendall('JOIN #'+CHANNELINIT + "\r\n") #Joins default channel
+					
+				if line.split(':')[2] == NICK + " quitplz":
+					s.sendall("PRIVMSG " + line.split(' ')[2] + " :Adios muchachos")
+					s.sendall("QUIT")
+					s.close()
+					sys.exit(0)
 				
-			if line.find("Hello " + NICK) != -1:
-				s.send("Hello! :) I am nex-bot <3")
-			line = s.recv(512)		
-		s.close()
-		print "Exiting now"
-		exit()
-	except Exception, err:
-		print "MY ERROR: " + str(err)
-		s.close()
-		exit()
-except KeyboardInterrupt:
-	print "Forced exit from user"
-	if s:
-		s.close()
-	sys.exit(0)
+				if line.find("Hello " + NICK) != -1:
+					print("PRIVMSG " + line.split(' ')[2] + " :Hello! :) I am nex-bot <3")
+					s.sendall("PRIVMSG " + line.split(' ')[2] + " :Hello! :) I am nex-bot <3")
+				count = 1
+				count = count + 1
+				line = s.recv(512)		
+			s.close()
+			print "Exiting now"
+			exit()
+		except Exception, err:
+			print "MY ERROR: " + str(err)
+			s.close()
+			sys.exit(0)
+	except KeyboardInterrupt:
+		print "Forced exit from user"
+		if s:
+			s.sendall("QUIT")
+			s.close()
+		sys.exit(0)
